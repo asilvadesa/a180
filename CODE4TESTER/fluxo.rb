237f4472404071e0715094e7451e364e99a5d136
Anderson de Sa <asilvadesa@gmail.com>
2020-12-22 23:06:12 -0400

    Add controle de fluxo

---
+++ CODE4TESTER/fluxo.rb
@@ -0,0 +1,11 @@
+idade = 18
+nome = "Anderson Sa"
+
+if(idade >= 18)
+  puts nome + ", você já pode tirar a CNH"
+elsif (idade >=7)
+  puts nome + ", você pode andar somente de bicicleta"
+else
+  puts nome + ", pode andar apenas a pé"
+end
+
