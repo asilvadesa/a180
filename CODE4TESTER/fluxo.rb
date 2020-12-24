8a2f1a1c9f0faba40644dadc83342f9a02d374f4
Anderson de Sa <asilvadesa@gmail.com>
2020-12-24 10:54:51 -0400

    Interpolacao de strings

--- CODE4TESTER/fluxo.rb
+++ CODE4TESTER/fluxo.rb
@@ -5,10 +5,10 @@ puts "Informe a sua idade"
 idade = gets.chomp.to_i
 
 if(idade >= 18)
-  puts nome + ", você já pode tirar a CNH"
+  puts "#{nome}" + ", você tem #{idade} anos e já pode tirar a CNH"
 elsif (idade >=7)
-  puts nome + ", você pode andar somente de bicicleta"
+  puts "#{nome}" + ", você tem #{idade} anos e pode andar somente de bicicleta"
 else
-  puts nome + ", pode andar apenas a pé"
+  puts "#{nome}" + ", você tem #{idade} anos e pode andar apenas a pé"
 end
 
