9ddc85a2978135e2d4dff9d141a68ce1d02975ac
Anderson de Sa <asilvadesa@gmail.com>
2020-12-24 16:10:58 -0400

    Trabalhando com Hashes

---
+++ CODE4TESTER/hashes.rb
@@ -0,0 +1,12 @@
+
+anderson = {nome: "Anderson Silva de Sa", email: "asilvadesa@gmail.com", legal: true }
+maria = {nome: "Maria de Sa", email: "maria@gmail.com", legal: true }
+
+# puts pessoa[:nome]
+# puts pessoa[:email]
+# puts pessoa[:legal]
+
+pessoas = [anderson, maria]
+
+puts pessoas[0]
+puts pessoas[1]
