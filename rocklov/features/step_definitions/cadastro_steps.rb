1769afeafc7680dbb901bc2f95dda4b2b56281e5
Anderson de Sa <asilvadesa@gmail.com>
2020-12-27 11:46:09 -0400

    Gerando email com faker

--- rocklov/features/step_definitions/cadastro_steps.rb
+++ rocklov/features/step_definitions/cadastro_steps.rb
@@ -4,7 +4,7 @@ end
 
 Quando('submeto o meu cadastro completo') do
   find("#fullName").set "Anderson Silva de Sa"
-  find("#email").set "asilvadesa@gmail.com"
+  find("#email").set Faker::Internet.free_email
   find("#password").set "pwd123"
 
   click_button "Cadastrar"
--- rocklov/features/support/env.rb
+++ rocklov/features/support/env.rb
@@ -1,5 +1,6 @@
 require "capybara"
 require "capybara/cucumber"
+require "faker"
 
 Capybara.configure do |config|
   config.default_driver = :selenium_chrome
