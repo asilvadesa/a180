142aca32dca084e3aa47b76ab92f61c6a4cbe268
Anderson de Sa <asilvadesa@gmail.com>
2020-12-27 12:17:46 -0400

    Adicionando o caso de teste para o cenario para cadastro sem nome

--- rocklov/features/step_definitions/cadastro_steps.rb
+++ rocklov/features/step_definitions/cadastro_steps.rb
@@ -14,3 +14,15 @@ Então('sou redirecionado para o dashboard') do
   expect(page).to have_css ".dashboard"
 end
 
+Quando('submeto o meu cadastro sem o nome') do
+  find("#email").set Faker::Internet.free_email
+  find("#password").set "pwd123"
+
+  click_button "Cadastrar"
+end
+
+Então('vejo a mensagem de alerta: Oops. Informe seu nome completo!') do
+  alert = find(".alert-dark")
+  expect(alert.text).to eql "Oops. Informe seu nome completo!"
+end
+
