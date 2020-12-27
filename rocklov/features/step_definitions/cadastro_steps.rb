63df88207a1b756d296b68c569bf2d322090c9b4
Anderson de Sa <asilvadesa@gmail.com>
2020-12-27 11:28:12 -0400

    Automatizando o primeiro cenario de teste com sucesso para a funcionalidade cadastro

--- rocklov/features/step_definitions/cadastro_steps.rb
+++ rocklov/features/step_definitions/cadastro_steps.rb
@@ -1,12 +1,16 @@
 Dado('que acesso a pagina de cadastro') do
-  visit "http://rocklov-web:3000/singup"
+  visit "http://rocklov-web:3000/signup"
 end
 
 Quando('submeto o meu cadastro completo') do
-  pending # Write code here that turns the phrase above into concrete actions
+  find("#fullName").set "Anderson Silva de Sa"
+  find("#email").set "asilvadesa@gmail.com"
+  find("#password").set "pwd123"
+
+  click_button "Cadastrar"
 end
 
 Então('sou redirecionado para o dashboard') do
-  pending # Write code here that turns the phrase above into concrete actions
+  expect(page).to have_css ".dashboard"
 end
 
