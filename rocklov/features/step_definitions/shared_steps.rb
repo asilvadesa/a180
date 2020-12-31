ac59c1ebde131462d05a1bd4eb5325a857a00a10
Anderson de Sa <asilvadesa@gmail.com>
2020-12-31 12:26:14 -0400

    Adicionando metodo que verifica as mensagem de alerta

--- rocklov/features/step_definitions/shared_steps.rb
+++ rocklov/features/step_definitions/shared_steps.rb
@@ -2,3 +2,7 @@ Então('sou redirecionado para o dashboard') do
   expect(page).to have_css ".dashboard"
 end
 
+Então('vejo a mensagem de alerta: {string}') do |alert_expect|
+  alert = find(".alert-dark")
+  expect(alert.text).to eql alert_expect
+end
