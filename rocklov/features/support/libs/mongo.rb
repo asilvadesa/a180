bbcbefd834d504918a23672b826fbc3cc99b3023
Anderson de Sa <asilvadesa@gmail.com>
2021-01-03 00:09:58 -0400

    Adicionando metodo initialize

--- rocklov/features/support/libs/mongo.rb
+++ rocklov/features/support/libs/mongo.rb
@@ -4,24 +4,27 @@ Mongo::Logger.logger = Logger.new("./logs/mongo.log")
 
 class MongoDB
 
-  def remove_user(email)
+  attr_accessor :users, :equipos
+
+  def initialize
     client = Mongo::Client.new('mongodb://rocklov-db:27017/rocklov')
-    users_collection = client[:users]
-    users_collection.delete_many({email: email})
+    @users = client[:users]
+    @equipos = client[:equipos]
+  end
+
+  def remove_user(email)
+
+    @users.delete_many({email: email})
   end
 
   def get_user(email)
-    client = Mongo::Client.new('mongodb://rocklov-db:27017/rocklov')
-    users = client[:users]
-    user = users.find({email: email}).first
+    user = @users.find({email: email}).first
     return user[:_id]
   end
 
   def remove_equipo(name, email)
     user_id = get_user(email)
-    client = Mongo::Client.new('mongodb://rocklov-db:27017/rocklov')
-    equipos = client[:equipos]
-    equipos.delete_many({name: name, user: user_id})
+    @equipos.delete_many({name: name, user: user_id})
   end
 end
 
