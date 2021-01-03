74cbad97b4599b03da9a13fb3c215403c73f6bd1
Anderson de Sa <asilvadesa@gmail.com>
2021-01-02 23:46:34 -0400

    Trabalhando com construtores

--- code4tester/oo/carro.rb
+++ code4tester/oo/carro.rb
@@ -2,16 +2,18 @@
 class Carro
   attr_accessor :nome, :cor, :marca
 
+  def initialize(nome, marca)
+    @nome = nome
+    @marca = marca
+  end
+
   def ligar
-    puts "O #{@nome} está pronto para inciar o trajeto"
+    puts "#{@nome}/#{@marca}: está pronto para inciar o trajeto"
   end
 end
 
-fusca = Carro.new
-fusca.nome = "Fusca"
+fusca = Carro.new("Fusca", "VW")
 fusca.ligar
 
-
-palio = Carro.new
-palio.nome = "Palio"
+palio = Carro.new("Palio", "Fiat")
 palio.ligar
