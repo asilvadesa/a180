60780b5a1a822f8cf33028f7f31e8b2fad5342fc
Anderson de Sa <asilvadesa@gmail.com>
2021-01-12 17:36:30 -0400

    Anexando Evidencias

--- rocklov/features/support/hooks.rb
+++ rocklov/features/support/hooks.rb
@@ -7,3 +7,12 @@ Before do
 
   page.current_window.resize_to(1440, 900)
 end
+
+After do
+  temp_shot = page.save_screenshot("logs/temp_screenshot.png")
+  Allure.add_attachment(
+    name: "Screenshot",
+    type: Allure::ContentType::PNG,
+    source: File.open(temp_shot)
+  )
+end
