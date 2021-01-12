124a949fff0d06b7ce433b091239ea76491e7e98
Anderson de Sa <asilvadesa@gmail.com>
2021-01-12 10:52:27 -0400

    Configurando relatorios com Allure

--- rocklov/features/support/env.rb
+++ rocklov/features/support/env.rb
@@ -1,3 +1,4 @@
+require "allure-cucumber"
 require "capybara"
 require "capybara/cucumber"
 require "faker"
@@ -6,4 +7,9 @@ Capybara.configure do |config|
   config.default_driver = :selenium_chrome
   config.app_host = "http://rocklov-web:3000"
   config.default_max_wait_time = 10
+end
+
+AllureCucumber.configure do |config|
+  config.results_directory = "/logs"
+  config.clean_results_directory = true
 end
