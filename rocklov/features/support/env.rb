ba9839a429c87047013015a36704e9873460261d
Anderson de Sa <asilvadesa@gmail.com>
2021-01-12 22:09:46 -0400

    Executando testes em outros ambientes

--- rocklov/features/support/env.rb
+++ rocklov/features/support/env.rb
@@ -3,9 +3,11 @@ require "capybara"
 require "capybara/cucumber"
 require "faker"
 
+CONFIG = YAML.load_file(File.join(Dir.pwd, "features/support/config/#{ENV["CONFIG"]}"))
+
 Capybara.configure do |config|
   config.default_driver = :selenium_chrome
-  config.app_host = "http://rocklov-web:3000"
+  config.app_host = CONFIG["url"]
   config.default_max_wait_time = 10
 end
 
--- rocklov/features/support/libs/mongo.rb
+++ rocklov/features/support/libs/mongo.rb
@@ -7,7 +7,7 @@ class MongoDB
   attr_accessor :users, :equipos
 
   def initialize
-    client = Mongo::Client.new('mongodb://rocklov-db:27017/rocklov')
+    client = Mongo::Client.new(CONFIG["mongo"])
     @users = client[:users]
     @equipos = client[:equipos]
   end
