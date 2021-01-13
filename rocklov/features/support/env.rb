51aee1d744c77085c92ea66d0a4db853c7c6c352
Anderson de Sa <asilvadesa@gmail.com>
2021-01-12 22:26:24 -0400

    Executando testes no Firefox

--- rocklov/features/support/env.rb
+++ rocklov/features/support/env.rb
@@ -6,7 +6,7 @@ require "faker"
 CONFIG = YAML.load_file(File.join(Dir.pwd, "features/support/config/#{ENV["CONFIG"]}"))
 
 Capybara.configure do |config|
-  config.default_driver = :selenium_chrome
+  config.default_driver = :selenium
   config.app_host = CONFIG["url"]
   config.default_max_wait_time = 10
 end
