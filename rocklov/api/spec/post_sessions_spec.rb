4557da97a77bd53b6d0bd924a53c628413055b42
Anderson de Sa <asilvadesa@gmail.com>
2021-01-28 00:13:21 -0400

    Contextos do RSpec

--- rocklov/api/spec/post_sessions_spec.rb
+++ rocklov/api/spec/post_sessions_spec.rb
@@ -1,17 +1,22 @@
 require "httparty"
 describe "POST /sessions" do
-  it "login sucess" do
-
-    payload = {email: "asilvagit@gmail.com", password: "pwd123"}
-
-    result = HTTParty.post(
+  context "login com sucesso" do
+    before(:all) do
+      payload = {email: "asilvagit@gmail.com", password: "pwd123"}
+      @result = HTTParty.post(
         "http://rocklov-api:3333/sessions",
         body: payload.to_json,
         headers: {
           "Content-Type": "application/json"
         }
       )
-    expect(result.code).to eql 200
-    expect(result.parsed_response["_id"].length).to eql 24
+    end
+    it "valida status code" do
+      expect(@result.code).to eql 200
+    end
+
+    it 'valida id do usuario' do
+      expect(@result.parsed_response["_id"].length).to eql 24
+    end
   end
 end
