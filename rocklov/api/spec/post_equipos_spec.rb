6a4dd49e17c32d0486640adaa98f9fba3888b423
Anderson de Sa <asilvadesa@gmail.com>
2021-01-30 14:58:33 -0400

    Automatizando POST Equipo

---
+++ rocklov/api/spec/post_equipos_spec.rb
@@ -0,0 +1,28 @@
+describe "POST /equipos" do
+  before(:all) do
+    payload = {email: "asilvagit@gmail.com", password: "pwd123"}
+    result = Sessions.new.login(payload)
+    @user_id = result.parsed_response["_id"]
+  end
+
+  context "Novo equipo" do
+    before(:all) do
+
+      thumbnail = File.open(File.join(Dir.pwd, "spec/fixtures/images", "kramer.jpg"))
+
+      payload = {
+        "thumbnail": thumbnail,
+        "name": "Kramer",
+        "category": "Cordas",
+        "price": 299
+      }
+      @result = Equipos.new.create(payload, @user_id)
+    end
+
+
+
+    it "deve retornar 200" do
+      expect(@result.code).to eql 200
+    end
+  end
+end
