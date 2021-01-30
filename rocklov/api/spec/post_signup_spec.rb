ce61247a409ef1162780e25cc9f3fee862aa64b7
Anderson de Sa <asilvadesa@gmail.com>
2021-01-29 23:45:59 -0400

    Usuario Duplicado

--- rocklov/api/spec/post_signup_spec.rb
+++ rocklov/api/spec/post_signup_spec.rb
@@ -17,4 +17,49 @@ describe "POST /signup" do
       expect(@result.parsed_response["_id"].length).to eql 24
     end
   end
+
+  examples = [
+    {
+      title: "Usuario ja existe",
+      payload: {name: "Betao", email: "betao@ig.com.br", password: "pwd123"},
+      code: 409,
+      error: "Email already exists :("
+    },
+    {
+      title: "Nome Vazio",
+      payload: {name: "", email: "betao@ig.com.br", password: "pwd123"},
+      code: 412,
+      error: "required name"
+    },
+    {
+      title: "Email vazio",
+      payload: {name: "Betao", email: "", password: "pwd123"},
+      code: 412,
+      error: "required email"
+    },{
+      title: "Senha Vazia",
+      payload: {name: "Betao", email: "betao@ig.com.br", password: ""},
+      code: 412,
+      error: "required password"
+    }
+  ]
+
+  examples.each do |e|
+    context "#{e[:title]}" do
+      before(:all) do
+        email = e[:payload]
+        MongoDB.new.remove_user(email[:email])
+        Signup.new.create(e[:payload])
+        @result = Signup.new.create(e[:payload])
+      end
+
+      it "valida status code #{e[:code]}" do
+        expect(@result.code).to eql e[:code]
+      end
+
+      it 'valida id do usuario' do
+        expect(@result.parsed_response["error"]).to eql e[:error]
+      end
+    end
+  end
 end
