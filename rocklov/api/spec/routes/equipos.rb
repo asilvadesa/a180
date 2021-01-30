726e71d7bcb74d6f71a4c54ab34616e7b2f79cfb
Anderson de Sa <asilvadesa@gmail.com>
2021-01-30 16:12:46 -0400

    Automatizando GET Equipo

--- rocklov/api/spec/routes/equipos.rb
+++ rocklov/api/spec/routes/equipos.rb
@@ -11,4 +11,13 @@ class Equipos < BaseApi
       }
     )
   end
+
+  def find_by_id(equipo_id, user_id)
+    return self.class.get(
+      "/equipos/#{equipo_id}",
+      headers: {
+        "user_id": user_id,
+      }
+    )
+  end
 end
