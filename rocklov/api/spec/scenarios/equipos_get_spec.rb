7c74e13110a5ddff5e82ce89f6105a764cd76ed1
Anderson de Sa <asilvadesa@gmail.com>
2021-01-30 16:28:53 -0400

    Movido para pasta scnearios

--- rocklov/api/spec/scenarios/equipos_get_spec.rb
+++ rocklov/api/spec/scenarios/equipos_get_spec.rb
@@ -31,4 +31,14 @@ describe "GET /equipos/{equipos_id}" do
       expect(@result.parsed_response).to include("name" => @payload[:name])
     end
   end
+
+  context "Equipo nao existe" do
+    before(:all) do
+      @result = Equipos.new.find_by_id(MongoDB.new.get_mongo_id, @user_id)
+    end
+
+    it "deve retornar 404" do
+      expect(@result.code).to eql 404
+    end
+  end
 end
